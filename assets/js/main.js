d8f43e06679b44eb61fae06e927d18d4c7b20934
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-09-12 23:31:45 -0300

    Header menu and unavaiable button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
-window.onload = function () {
-	'use strict';
- 	// window.alert('hi');
-};
+'use strict';
+(function(){
+	if( !window.app ){
+		window.app = {};
+	}
+})();
+
+
+(function(){
+	window.app.openMenu = function(){
+		var btn_menu = document.querySelectorAll('#header .item-menu a')[0];
+		var menu = document.querySelector('#main-menu');
+
+		btn_menu.addEventListener('click', function(e){
+			e.preventDefault();
+			menu.classList.add('actived');
+		});
+	};
+})();
+
+document.addEventListener("DOMContentLoaded", function(e) {
+	window.app.openMenu();	
+});
